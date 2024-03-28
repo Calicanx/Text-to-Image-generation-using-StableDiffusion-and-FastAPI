2153ede7a4e917015aa016d318b9c859925cd478
0xSc0rpi0n <55562628+Calicanx@users.noreply.github.com>
2024-03-28 19:35:38 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer {API_token}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
